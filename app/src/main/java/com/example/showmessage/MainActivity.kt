c8a1e808b8a6c847e7e45c7ecbca41054d12565f
Rodrigo <00157219@uca.edu.sv>
2021-08-07 15:15:56 -0600

    App done

--- app/src/main/java/com/example/showmessage/MainActivity.kt
+++ app/src/main/java/com/example/showmessage/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.showmessage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var showMessage: TextView
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bind()
+        showMessage()
+    }
+
+   private fun bind() {
+        editText = findViewById(R.id.entry_message)
+        showMessage = findViewById(R.id.txt_message)
+        button = findViewById(R.id.btn_action)
+    }
+
+    private fun showMessage() {
+        button.setOnClickListener {
+            showMessage.text = editText.text.toString()
+        }
     }
 }
